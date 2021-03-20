2614d965a7e10fa5db21b0a05ba0bf3151135675
pedro1559 <78930227+pedro1559@users.noreply.github.com>
2021-03-20 06:11:35 -0300

    Add files via upload

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (pedrobot3.0)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (20/03,05:50AM)
+ *
+ * author                (Pedro)
+ * Copyright notice      (bot com suporte a Android/Java)
